93cde945dea34a285055a37f61643493d6af62d0
A.Sweilam <anhsbio@gmail.com>
2020-07-03 23:24:48 +0100

    Added routines to move the player to different locations.

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -5,19 +5,36 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+    private static Map<Integer, Location> locations = new HashMap<>();
 
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
 
         locations.put(0, new Location(0, "You're in front of the computer"));
-        locations.put(1, new Location(1, "You're at end of road with rock"));
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building.\n" +
+                "Around you is a forest."));
         locations.put(2, new Location(2, "You're at top of a hill"));
-        locations.put(3, new Location(3, "inside a building"));
-        locations.put(4, new Location(4, "in a valley"));
+        locations.put(3, new Location(3, "You are inside a building, a well house for a large spring."));
+        locations.put(4, new Location(4, "YOU ARE IN A VALLEY BESIDE A STREAM TUMBLING ALONG A    \n" +
+                "ROCKY BED."));
         locations.put(5, new Location(5, "in the forest"));
 
+        locations.get(1).addExist("W", 2);
+        locations.get(1).addExist("E", 3);
+        locations.get(1).addExist("S", 4);
+        locations.get(1).addExist("N", 5);
+
+        locations.get(2).addExist("N", 5);
+
+
+        locations.get(3).addExist("W", 1);
+
+        locations.get(4).addExist("N", 1);
+        locations.get(4).addExist("W", 2);
+
+        locations.get(5).addExist("S", 1);
+        locations.get(5).addExist("W", 2);
 
 
         int loc = 1;
@@ -26,9 +43,22 @@ public class Main {
 
             if (loc ==0)
                 break;
-            loc = scanner.nextInt();
-            if (!locations.containsKey(loc)){
-                System.out.println("You can't go in that direction");
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.println("Available exists are: ");
+
+            for (String exist: exits.keySet()
+                 ) {
+                System.out.print(exist + ", ");
+            }
+
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+
+            if (exits.containsKey(direction)){
+                loc = exits.get(direction);
+            } else {
+                System.out.println("Wrong direction");
             }
         }
 
