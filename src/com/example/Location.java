6d855b51f25b57dbd6f974f427f7cbd51237eaa8
A.Sweilam <anhsbio@gmail.com>
2020-07-04 23:38:30 +0100

    Turning Location class into immutable class. Created new temp hashmap to hold the exits. removed addExits method.

--- src/com/example/Location.java
+++ src/com/example/Location.java
@@ -8,16 +8,16 @@ public class Location {
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<>();
+        this.exits = exits;
         this.exits.put("Q", 0);
     }
 
-    protected void addExist(String direction, int location) {
-        exits.put(direction, location);
-    }
+//    protected void addExist(String direction, int location) {
+//        exits.put(direction, location);
+//    }
 
     protected int getLocationID() {
         return locationID;
--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -11,30 +11,36 @@ public class Main {
         // write your code here
         Scanner scanner = new Scanner(System.in);
 
-        locations.put(0, new Location(0, "You're in front of the computer"));
+        Map<String, Integer> tempExists = new HashMap<>();
+        locations.put(0, new Location(0, "You're in front of the computer", tempExists));
+
+        tempExists = new HashMap<>();
+        tempExists.put("W", 2);
+        tempExists.put("E", 3);
+        tempExists.put("S", 4);
+        tempExists.put("N", 5);
         locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building.\n" +
-                "Around you is a forest."));
-        locations.put(2, new Location(2, "You're at top of a hill"));
-        locations.put(3, new Location(3, "You are inside a building, a well house for a large spring."));
+                "Around you is a forest.", tempExists));
+
+        tempExists = new HashMap<>();
+        tempExists.put("N", 5);
+        locations.put(2, new Location(2, "You're at top of a hill", tempExists));
+
+        tempExists = new HashMap<>();
+        tempExists.put("W", 1);
+        locations.put(3, new Location(3, "You are inside a building, a well house for a large spring.", tempExists));
+
+        tempExists = new HashMap<>();
+        tempExists.put("N", 1);
+        tempExists.put("W", 2);
         locations.put(4, new Location(4, "YOU ARE IN A VALLEY BESIDE A STREAM TUMBLING ALONG A    \n" +
-                "ROCKY BED."));
-        locations.put(5, new Location(5, "in the forest"));
+                "ROCKY BED.", tempExists));
 
-        locations.get(1).addExist("W", 2);
-        locations.get(1).addExist("E", 3);
-        locations.get(1).addExist("S", 4);
-        locations.get(1).addExist("N", 5);
-
-        locations.get(2).addExist("N", 5);
-
-
-        locations.get(3).addExist("W", 1);
-
-        locations.get(4).addExist("N", 1);
-        locations.get(4).addExist("W", 2);
-
-        locations.get(5).addExist("S", 1);
-        locations.get(5).addExist("W", 2);
+        tempExists = new HashMap<>();
+        tempExists.put("S", 1);
+        tempExists.put("W", 2);
+        locations.put(5, new Location(5, "in the forest",tempExists));
+        
 
         Map<String, String> options = Map.of("W", "WEST",
                 "E", "EAST",
